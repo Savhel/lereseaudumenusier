1b7f9884619ebdb94551aea2ca2c7810526acff0
Savhel <103961239+Savhel@users.noreply.github.com>
2023-10-04 21:04:39 +0100

    Update CategoryNav.js

--- src/components/CategoryNav.js
+++ src/components/CategoryNav.js
@@ -32,14 +32,14 @@ useEffect(() => {
   };
 
 
- // console.log(categories)
+ // console.log(categories) 
 
 
   return(
     <aside className='hidden xl:flex'>
       <div className='bg-primary flex flex-col w-[286px] h-[500px] rounded-[8px] overflow-hidden'>
         <div className='bg-accent py-4 text-primary font-semibold uppercase flex items-center justify-center'>Catégories</div>
-        <div className='flex flex-col gap-y-6 p-6'>
+        <div className='flex flex-col gap-y-6 p-6 overflow-y-auto h-[70vh]'>
           {categories?.map((categorie) => {
            // console.log(categorie.nom);
             return <Link to={`/products/${categorie.id}`} className='cursor-pointer uppercase' key={categorie.id}>{categorie.nom}</Link>;
